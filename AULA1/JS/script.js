1d49cc55ccaf811f6ad6ce0de8c5a952df736d5b
samyev <sam1967evelyn@gmail.com>
2021-03-22 18:27:02 -0300

    resolvendo erros

--- AULA1/JS/script.js
+++ AULA1/JS/script.js
@@ -1,4 +1,4 @@
-var valorEmReal = prompt("Qual valor em real que você deseja converter?");
+var valorEmReal = prompt("Qual valor em bitcoin que você deseja converter?");
 valorEmReal = parseFloat(valorEmReal);
 
 var valorEmBitCoin = valorEmReal * 300520.69;
