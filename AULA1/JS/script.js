6dbf08d8ed8d84921134ba3a298a923500c75326
samyev <sam1967evelyn@gmail.com>
2021-03-22 19:05:13 -0300

    resolvendo erros de semantica nos projetos

--- AULA1/JS/script.js
+++ AULA1/JS/script.js
@@ -4,4 +4,4 @@ valorEmDolar = parseFloat(valorEmDolar);
 var valorEmReal = valorEmDolar * 5.51;
 var valorRealFixado = valorEmReal.toFixed(2);
 
-alert("A Conversão do valor " +valorEmDolar+ " em real é: " +valorRealFixado);
+alert("A Conversão do valor " +valorEmDolar+ " em bitcoin é: " +valorRealFixado);
--- Desafios/ConversorReal-BitCoin/JS/script.js
+++ Desafios/ConversorReal-BitCoin/JS/script.js
@@ -1,7 +1,8 @@
 var valorEmReal = prompt("Qual valor em bitcoin que você deseja converter?");
+
 valorEmReal = parseFloat(valorEmReal);
 
-var valorEmBitCoin = valorEmReal * 300520.69;
-var valorBitCoinFixado = valorEmBitCoin.toFixed(2);
+var valorEmBitCoin = valorEmReal * 0.0000033;
+var valorBitCoinFixado = valorEmBitCoin.toFixed(7);
 
-alert("A Conversão do valor " +valorEmReal+ " em real é: " +valorBitCoinFixado);
+alert("A Conversão do valor " +valorEmReal+ " em bitcoin é: " +valorBitCoinFixado);
