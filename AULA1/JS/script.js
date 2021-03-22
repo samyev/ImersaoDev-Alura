c1ef05785d93f4e593f923fa69341cb216faa1f0
samyev <sam1967evelyn@gmail.com>
2021-03-22 18:11:05 -0300

    adicionando o primeiro desafio

---
+++ AULA1/JS/script.js
@@ -0,0 +1,7 @@
+var valorEmReal = prompt("Qual valor em real que você deseja converter?");
+valorEmReal = parseFloat(valorEmReal);
+
+var valorEmBitCoin = valorEmReal * 300520.69;
+var valorBitCoinFixado = valorEmBitCoin.toFixed(2);
+
+alert("A Conversão do valor " +valorEmReal+ " em real é: " +valorBitCoinFixado);
